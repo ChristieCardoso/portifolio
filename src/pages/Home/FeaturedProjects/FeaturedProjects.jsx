dd5d425f8c6ae2d4390916e7bbf67be2daf4985f
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-19 20:09:32 -0300

    add useState useEffect

--- src/pages/Home/FeaturedProjects/FeaturedProjects.jsx
+++ src/pages/Home/FeaturedProjects/FeaturedProjects.jsx
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Project from '../../../data/projects'
+import Project from '../../../data/projects';
 import ProjectCard from './ProjectCard/ProjectCard';
 
 import './FeaturedProjects.scss';
 
 export const FeaturedProjects = () => {
+  const [featuredProjects, setFeaturedProjects] = useState([]);
+
+  useEffect(() => {
+    // Buscando os projetos em destaque ou realizar outras operações assíncronas
+    const fetchFeaturedProjects = () => {
+      const filteredProjects = Project.filter((project) => project.id === '1' || project.id === '4');
+      setFeaturedProjects(filteredProjects);
+    };
+
+    fetchFeaturedProjects();
+  }, []);
+
   return (
     <div className='FeaturedProjectContainer'>
       <h1 className='FeaturedProjectTitle'>
@@ -12,15 +25,13 @@ export const FeaturedProjects = () => {
       </h1>
 
       <section className='FeaturedProject'>
-        {Project.filter((props) => props.id === '1' || props.id === '3').map((props) => (
-          <ProjectCard key={props.id} {...props} />
+        {featuredProjects.map((project) => (
+          <ProjectCard key={project.id} {...project} />
         ))}
       </section>
       <Link to="/project" className="HomeBtn">
         Veja Mais
       </Link>
-
     </div>
   );
 };
-
