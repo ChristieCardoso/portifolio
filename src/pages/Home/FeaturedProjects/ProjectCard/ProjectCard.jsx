f0b9f3f983f36b980cd6ba1a2cb7f3d4022dd644
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-19 21:33:18 -0300

    Simplify ProjectCard component and use ternary conditionals for link rendering

--- src/pages/Home/FeaturedProjects/ProjectCard/ProjectCard.jsx
+++ src/pages/Home/FeaturedProjects/ProjectCard/ProjectCard.jsx
@@ -1,31 +1,25 @@
-import { FaRegEye, FaGithub } from "react-icons/fa";
+import { FaRegEye, FaGithub } from 'react-icons/fa';
 
 import './ProjectCard.scss';
 
-const ProjectCard = ({image, title, description, links }) => {
-
-  return (
-    <section className='FeaturedProjectCardContainer'>
-      <div className='FeaturedProjectCard'>
-        <img
-          alt="card-img"
-          src={image.src}
-          width="100%"
-        />
-
-        <div className='FeaturedProjectCardBody'>
-          <h3 className='FeaturedProjectCardTitle'>{title}</h3>
-          <p className='FeaturedProjectDescription'>{description}</p>
-          <span className="FeaturedProjectBtnContainer">
-            {links.site ? <a className='projectBtn' href={links.site} target='_blank'><FaRegEye /></a> : null}
-            {links.repo ? <a className='projectBtn' href={links.repo} target='_blank'><FaGithub /></a> : null}
-          </span>
-
-        </div>
-
+const ProjectCard = ({ image, title, description, links }) => (
+  <section className='FeaturedProjectCardContainer'>
+    <div className='FeaturedProjectCard'>
+      <img
+        alt="card-img"
+        src={image.src}
+        width="100%"
+      />
+      <div className='FeaturedProjectCardBody'>
+        <h3 className='FeaturedProjectCardTitle'>{title}</h3>
+        <p className='FeaturedProjectDescription'>{description}</p>
+        <span className="FeaturedProjectBtnContainer">
+          {links.site && <a className='projectBtn' href={links.site} target='_blank'><FaRegEye /></a>} {/* Usa a condição ternária para renderizar o link apenas se links.site existir */}
+          {links.repo && <a className='projectBtn' href={links.repo} target='_blank'><FaGithub /></a>}
+        </span>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default ProjectCard;
