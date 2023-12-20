fcf1c0989299ce984e63f95c9959df4f773b6424
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-19 21:06:06 -0300

    Integrate interactive filtering for featured projects

--- src/pages/Home/FeaturedProjects/FeaturedProjects.jsx
+++ src/pages/Home/FeaturedProjects/FeaturedProjects.jsx
@@ -6,23 +6,34 @@ import ProjectCard from './ProjectCard/ProjectCard';
 import './FeaturedProjects.scss';
 
 export const FeaturedProjects = () => {
+  const [allProjects, setAllProjects] = useState([]); // Estado para armazenar todos os projetos
   const [featuredProjects, setFeaturedProjects] = useState([]);
+  const [filter, setFilter] = useState('all'); // Estado para armazenar o filtro selecionado
 
   useEffect(() => {
-    // Buscando os projetos em destaque ou realizar outras operações assíncronas
-    const fetchFeaturedProjects = () => {
-      const filteredProjects = Project.filter((project) => project.id === '1' || project.id === '4');
-      setFeaturedProjects(filteredProjects);
-    };
+    // Carregar todos os projetos ao inicializar o componente
+    setAllProjects(Project);
+    // Atualize os projetos em destaque sempre que o filtro for alterado
+    const filteredProjects = allProjects.filter((project) => filter === 'all' || project.category === filter);
+    setFeaturedProjects(filteredProjects);
+  }, [filter, allProjects]);
 
-    fetchFeaturedProjects();
-  }, []);
+  const handleFilterChange = (selectedFilter) => {
+    setFilter(selectedFilter);
+  };
 
   return (
     <div className='FeaturedProjectContainer'>
       <h1 className='FeaturedProjectTitle'>
         Projetos em Destaque
       </h1>
+
+      <div>
+        <button onClick={() => handleFilterChange('all')}>Destaques</button>
+        <button onClick={() => handleFilterChange('landing-page')}>LandingPage</button>
+        <button onClick={() => handleFilterChange('estudo')}>Estudos</button>
+        <button onClick={() => handleFilterChange('api')}>Api</button>
+      </div>
 
       <section className='FeaturedProject'>
         {featuredProjects.map((project) => (
@@ -35,3 +46,4 @@ export const FeaturedProjects = () => {
     </div>
   );
 };
+
